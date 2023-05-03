dc3eb0b642a12f513ad9b7c351e351fbb1758203
Jakub Praszkowski <kubapraszkowski@gmail.com>
2023-05-03 22:04:39 +0200

    add: error handling in htdocs script

--- update-htdocs.sh
+++ update-htdocs.sh
@@ -5,9 +5,25 @@
 REPO_URL="https://github.com/jakubpraszkowski/Dating-application-with-Pepe-memes"
 HTDOCS_DIR="~/Dating-application-with-Pepe-memes/htdocs"
 
-git clone --depth 1 $REPO_URL /tmp/repo
+# Clone the latest commit from the repository
+if git clone --depth 1 $REPO_URL /tmp/repo; then
+    # Replace the contents of the htdocs directory with the contents of the cloned repository
+    if rm -rf $HTDOCS_DIR/* && cp -r /tmp/repo/htdocs/* $HTDOCS_DIR/; then
+        echo "htdocs directory updated successfully."
+    else
+        echo "Failed to copy cloned files to htdocs directory." >&2
+        exit 1
+    fi
+else
+    echo "Failed to clone repository." >&2
+    exit 1
+fi
 
-rm -rf $HTDOCS_DIR/*
-cp -r /tmp/repo/htdocs/* $HTDOCS_DIR/
+# Clean up the temporary directory
+if rm -rf /tmp/repo; then
+    echo "Temporary directory cleaned up successfully."
+else
+    echo "Failed to clean up temporary directory." >&2
+    exit 1
+fi
 
-rm -rf /tmp/repo
