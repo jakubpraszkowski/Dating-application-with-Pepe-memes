b55818f43b98a811704f0e839db85e5189046c81
Emilia <kalanskae@gmail.com>
2023-05-03 13:35:07 +0200

    add: add_date and u_id fields in memes table, populated databese with some example data

--- htdocs/meme_dating.sql
+++ htdocs/meme_dating.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2023 at 07:56 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Czas generowania: 03 Maj 2023, 13:33
+-- Wersja serwera: 10.4.25-MariaDB
+-- Wersja PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `meme_dating`
+-- Baza danych: `meme_dating`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categories`
+-- Struktura tabeli dla tabeli `categories`
 --
 
 CREATE TABLE `categories` (
@@ -32,22 +32,50 @@ CREATE TABLE `categories` (
   `title` varchar(40) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Zrzut danych tabeli `categories`
+--
+
+INSERT INTO `categories` (`cat_id`, `title`) VALUES
+(1, 'cat1'),
+(2, 'cat2');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `memes`
+-- Struktura tabeli dla tabeli `memes`
 --
 
 CREATE TABLE `memes` (
   `m_id` int(11) NOT NULL,
   `url` text COLLATE utf8_bin NOT NULL,
-  `cat_id` int(11) NOT NULL
+  `cat_id` int(11) NOT NULL,
+  `u_id` int(11) NOT NULL,
+  `add_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Zrzut danych tabeli `memes`
+--
+
+INSERT INTO `memes` (`m_id`, `url`, `cat_id`, `u_id`, `add_date`) VALUES
+(1, 'https://i.imgur.com/u4h4OoK.jpeg', 1, 1, '2023-05-01'),
+(2, 'https://i.imgur.com/Li1OKSU.jpeg', 1, 1, '2023-05-01'),
+(3, 'https://i.imgur.com/2I8Wld5.jpeg', 1, 1, '2023-05-16'),
+(4, 'https://i.imgur.com/6b0wcn7.jpeg', 1, 1, '2023-05-26'),
+(5, 'https://i.imgur.com/eSEyP0I.jpeg', 1, 1, '2023-05-06'),
+(6, 'https://i.imgur.com/P978Mos.jpeg', 1, 1, '2023-05-09'),
+(7, 'https://i.imgur.com/Y8PYNHm.jpeg', 2, 1, '2023-05-03'),
+(8, 'https://i.imgur.com/Y8PYNHm.jpeg', 2, 1, '2023-05-03'),
+(9, 'https://i.imgur.com/8JeqfAO.jpeg', 2, 1, '2023-05-03'),
+(10, 'https://i.imgur.com/8JeqfAO.jpeg', 2, 1, '2023-05-03'),
+(11, 'https://i.imgur.com/i3PP4Hl.png', 2, 1, '2023-05-03'),
+(12, 'https://i.imgur.com/i3PP4Hl.png', 2, 1, '2023-05-03');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `messages`
+-- Struktura tabeli dla tabeli `messages`
 --
 
 CREATE TABLE `messages` (
@@ -61,7 +89,7 @@ CREATE TABLE `messages` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktura tabeli dla tabeli `users`
 --
 
 CREATE TABLE `users` (
@@ -73,10 +101,17 @@ CREATE TABLE `users` (
   `birthday` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`u_id`, `name`, `lastname`, `login`, `pass`, `birthday`) VALUES
+(1, 'coolUser', 'idk', 'a', 'a', '2023-04-03');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_preferences`
+-- Struktura tabeli dla tabeli `user_preferences`
 --
 
 CREATE TABLE `user_preferences` (
@@ -87,24 +122,25 @@ CREATE TABLE `user_preferences` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Indexes for dumped tables
+-- Indeksy dla zrzutów tabel
 --
 
 --
--- Indexes for table `categories`
+-- Indeksy dla tabeli `categories`
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`cat_id`);
 
 --
--- Indexes for table `memes`
+-- Indeksy dla tabeli `memes`
 --
 ALTER TABLE `memes`
   ADD PRIMARY KEY (`m_id`),
-  ADD KEY `cat_id` (`cat_id`);
+  ADD KEY `cat_id` (`cat_id`),
+  ADD KEY `u_id` (`u_id`);
 
 --
--- Indexes for table `messages`
+-- Indeksy dla tabeli `messages`
 --
 ALTER TABLE `messages`
   ADD PRIMARY KEY (`msg_id`),
@@ -112,13 +148,13 @@ ALTER TABLE `messages`
   ADD KEY `toid` (`to_id`);
 
 --
--- Indexes for table `users`
+-- Indeksy dla tabeli `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`u_id`);
 
 --
--- Indexes for table `user_preferences`
+-- Indeksy dla tabeli `user_preferences`
 --
 ALTER TABLE `user_preferences`
   ADD PRIMARY KEY (`pref_id`),
@@ -126,58 +162,58 @@ ALTER TABLE `user_preferences`
   ADD KEY `cat_id2` (`cat_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT dla zrzuconych tabel
 --
 
 --
--- AUTO_INCREMENT for table `categories`
+-- AUTO_INCREMENT dla tabeli `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `memes`
+-- AUTO_INCREMENT dla tabeli `memes`
 --
 ALTER TABLE `memes`
-  MODIFY `m_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `m_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT for table `messages`
+-- AUTO_INCREMENT dla tabeli `messages`
 --
 ALTER TABLE `messages`
   MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `user_preferences`
+-- AUTO_INCREMENT dla tabeli `user_preferences`
 --
 ALTER TABLE `user_preferences`
   MODIFY `pref_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Constraints for dumped tables
+-- Ograniczenia dla zrzutów tabel
 --
 
 --
--- Constraints for table `memes`
+-- Ograniczenia dla tabeli `memes`
 --
 ALTER TABLE `memes`
   ADD CONSTRAINT `cat_id` FOREIGN KEY (`cat_id`) REFERENCES `categories` (`cat_id`);
 
 --
--- Constraints for table `messages`
+-- Ograniczenia dla tabeli `messages`
 --
 ALTER TABLE `messages`
   ADD CONSTRAINT `fromid` FOREIGN KEY (`from_id`) REFERENCES `users` (`u_id`),
   ADD CONSTRAINT `toid` FOREIGN KEY (`to_id`) REFERENCES `users` (`u_id`);
 
 --
--- Constraints for table `user_preferences`
+-- Ograniczenia dla tabeli `user_preferences`
 --
 ALTER TABLE `user_preferences`
   ADD CONSTRAINT `cat_id2` FOREIGN KEY (`cat_id`) REFERENCES `categories` (`cat_id`);
