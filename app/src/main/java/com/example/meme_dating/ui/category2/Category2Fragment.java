1439e3bdc8b2ba161d21ff5e9f4cd1f06879cebb
Emilia <kalanskae@gmail.com>
2023-05-06 23:43:01 +0200

    added more categories, but cat 3-5 are empty, so dont open them

---
+++ app/src/main/java/com/example/meme_dating/ui/category2/Category2Fragment.java
@@ -0,0 +1,54 @@
+package com.example.meme_dating.ui.category2;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.meme_dating.R;
+import com.example.meme_dating.databinding.FragmentMemeScrollingBinding;
+import com.example.meme_dating.ui.categoryMain.CategoryMainFragment;
+import com.example.meme_dating.ui.categoryMain.CategoryMainViewModel;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+public class Category2Fragment extends CategoryMainFragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        cat_id = 2;
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                String[] field =  {"cat_id"};
+                String[] data = {String.valueOf(cat_id)};
+                PutData putData = new PutData("https://meme-dating.one.pl/memesInCategoryCheck.php", "POST", field, data);
+                if (putData.startPut()) {
+                    if (putData.onComplete()) {
+                        Log.d("meme", "memes in that category: "+putData.getResult());
+                        memesInThatCategory = Integer.parseInt(putData.getResult());
+                    }
+                }
+            }
+        });
+        CategoryMainViewModel categoryMainViewModel = new ViewModelProvider(this).get(CategoryMainViewModel.class);
+
+        binding = FragmentMemeScrollingBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        super.onCreate(savedInstanceState);
+
+        recyclerView = (RecyclerView) root.findViewById(R.id.recyclerView);
+
+        firstMemes();
+        initAdapter();
+        initScrollListener();
+
+        return root;
+    }
+}
