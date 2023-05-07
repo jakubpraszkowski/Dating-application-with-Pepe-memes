50159a8ed7a8d6265ab42dcfb112b75f6a5e5520
Weronika <wlodarczykweronika5@gmail.com>
2023-05-07 15:02:43 +0200

    Add layouts for messages senders and receivers

--- app/src/main/java/com/example/meme_dating/MainActivity.java
+++ app/src/main/java/com/example/meme_dating/MainActivity.java
@@ -7,7 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class MainActivity extends AppCompatActivity {
+
 
     Button ScrollMemesButton;
 
@@ -15,13 +20,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
 
         ScrollMemesButton = findViewById(R.id.button1);
         ScrollMemesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                myRef.setValue("Hello, World!");
                 startActivity(new Intent(MainActivity.this, CategoryMenuActivity.class));
             }
         });
     }
-}
+
+    }
