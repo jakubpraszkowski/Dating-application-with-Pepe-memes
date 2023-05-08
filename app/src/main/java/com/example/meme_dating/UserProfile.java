5ddba28c6ca2fe241312194cac7c87f3beb5d23f
Mateusz Wróbel <m.wrobel@xyz@gmail.com>
2023-05-08 22:20:20 +0200

    Working user profile.

--- app/src/main/java/com/example/meme_dating/UserProfile.java
+++ app/src/main/java/com/example/meme_dating/UserProfile.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -21,15 +22,17 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class UserProfile extends AppCompatActivity {
 
-    TextView usernameTextView, categoryTextView, pointsTextView;
+    TextView usernameTextView;
+    ListView categoryPointsListView;
     Button backButton;
-    ArrayList<String> memeCategories;
     ArrayAdapter<String> adapter;
+    List<String> categories = new ArrayList<>();
+    List<Integer> points = new ArrayList<>();
     String username;
-    int points;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,17 +46,11 @@ public class UserProfile extends AppCompatActivity {
                 onBackPressed();
             }
         });
-        // pobranie przekazanego ID użytkownika z intencji
-
 
         usernameTextView = findViewById(R.id.user_name);
-        categoryTextView = findViewById(R.id.categoryTextView);
-        pointsTextView = findViewById(R.id.pointsTextView);
-        usernameTextView = findViewById(R.id.user_name);
-        categoryTextView = findViewById(R.id.categoryTextView);
-        pointsTextView = findViewById(R.id.pointsTextView);
+        categoryPointsListView = findViewById(R.id.listViewMemes);
 
-// pobranie przekazanego ID użytkownika z intencji
+
         int userId = getIntent().getIntExtra("user_id", 0);
 
         Handler handler = new Handler();
@@ -62,23 +59,31 @@ public class UserProfile extends AppCompatActivity {
             field[0] = "u_id";
             String[] data = new String[1];
             data[0] = String.valueOf(userId);
-            PutData putData = new PutData("https://meme-dating.one.pl/LoginRegister/getUserProfile.php", "POST", field, data);
+            PutData putData = new PutData("http://10.0.2.2/getUserProfile.php", "POST", field, data);
+
+
             if (putData.startPut()) {
                 if (putData.onComplete()) {
                     String result = putData.getResult();
+
                     try {
                         JSONArray jsonArray = new JSONArray(result);
-                        int points = 0;
-                        String category = "";
+
                         for (int i = 0; i < jsonArray.length(); i++) {
                             JSONObject jsonObject = jsonArray.getJSONObject(i);
                             username = jsonObject.getString("username");
-                            category += jsonObject.getString("title") + "\n";
-                            points += jsonObject.getInt("points");
+                            String category = jsonObject.optString("title", "Brak kategorii");
+                            int point = jsonObject.optInt("points", -1);
+                            if(point > -1)
+                                categories.add("Kategoria: " + category + "\nZdobyte punkty:" + point );
+                            else categories.add("Brak osiągnięć!");
+
                         }
                         usernameTextView.setText(username);
-                        categoryTextView.setText(category);
-                        pointsTextView.setText(String.valueOf(points));
+
+                        adapter = new ArrayAdapter<>(UserProfile.this,
+                                android.R.layout.simple_list_item_1, categories);
+                        categoryPointsListView.setAdapter(adapter);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
