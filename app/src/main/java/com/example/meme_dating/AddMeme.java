4fbd85a5394e8e2793e7c7516ffc85332ff7db7c
Mateusz Wróbel <m.wrobel@xyz@gmail.com>
2023-05-09 18:23:23 +0200

    Update files.

--- app/src/main/java/com/example/meme_dating/AddMeme.java
+++ app/src/main/java/com/example/meme_dating/AddMeme.java
@@ -4,13 +4,28 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
 
 
 public class AddMeme extends AppCompatActivity {
@@ -21,6 +36,48 @@ public class AddMeme extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_meme);
+
+        try {
+            URL url = new URL("https://meme-dating.one.pl/getCategories.php");
+            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            int responseCode = conn.getResponseCode();
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                String result = response.toString();
+                JSONArray jsonArray = new JSONArray(result);
+
+                ArrayList<String> categories = new ArrayList<>();
+                ArrayList<Integer> cat_id = new ArrayList<>();
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String category = jsonObject.getString("title");
+                    categories.add(category);
+                }
+
+                Spinner categorySpinner = findViewById(R.id.category_spinner);
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
+                        android.R.layout.simple_spinner_item, categories);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                categorySpinner.setAdapter(adapter);
+
+            } else {
+                Toast.makeText(getBaseContext(), "Nie można połączyć się z serwerem", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         Button chooseFileButton = findViewById(R.id.choose_file_button);
         chooseFileButton.setOnClickListener(new View.OnClickListener() {
