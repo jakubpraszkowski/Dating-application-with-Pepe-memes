1007bc7d2a7f25062c8756795abab4131d43cce8
Mateusz Wróbel <m.wrobel@xyz@gmail.com>
2023-05-09 16:04:40 +0200

    Add add meme layout.

---
+++ app/src/main/java/com/example/meme_dating/AddMeme.java
@@ -0,0 +1,13 @@
+package com.example.meme_dating;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddMeme extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_meme);
+    }
+}
